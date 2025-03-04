fcca7f6ba000d783db08b03e1293dc877b391447
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2025-03-04 13:04:13 -0500

    Add extra spacing

--- R/checks.R
+++ R/checks.R
@@ -8,6 +8,7 @@ check_number_decimal_strict <- function(
   if ((x > min) & (x < max)) {
     return(invisible(NULL))
   }
+
   cli::cli_abort(
     "{.arg {arg}} must be a number strictly between {.num {min}} and {.num {max}}, not {x}.",
     arg = arg,
@@ -19,6 +20,7 @@ check_complete <- function(x, arg=rlang::caller_arg(x), call=rlang::caller_env()
   if (all(!is.na(x))) {
     return(invisible(NULL))
   }
+
   cli::cli_abort(
     "{.arg {arg}} cannot contain missing values.",
     arg = arg,
