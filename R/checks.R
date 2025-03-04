83f644b257cc9786c7ec43465e7988cd8d5d172b
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2025-03-04 16:32:13 -0500

    Fix handling of optional (NULL) column specifications

--- R/checks.R
+++ R/checks.R
@@ -1,3 +1,28 @@
+check_column_exists <- function(
+    df,
+    x,
+    allow_null = FALSE,
+    arg_df = rlang::caller_arg(df),
+    arg_x = rlang::caller_arg(x),
+    call = rlang::caller_env()
+) {
+  x <- rlang::enquo(x)
+
+  if(rlang::quo_is_null(x) & allow_null) {
+    return(invisible(NULL))
+  }
+  if (rlang::as_name(x) %in% names(df)) {
+    return(invisible(NULL))
+  }
+
+  cli::cli_abort(
+    "{.arg {arg_x}} must be a column in {.arg {arg_df}}.",
+    arg_df = arg_df,
+    arg_x = arg_x,
+    call = call
+  )
+}
+
 check_number_decimal_strict <- function(
   x,
   min,
